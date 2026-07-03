cb84225dac07accf4047b0767dbafee63dd530b9
pradhan-not-found <souradeeppradhan7@gmail.com>
2026-07-03 11:44:52 +0530

    datatypes in python

---
+++ datatypes.py
@@ -0,0 +1,25 @@
+#data types in python is the first thing we wwill learn
+#1 int 
+age = 32
+
+#2 float
+weight =72.50
+
+#3 string
+name='souradeep'
+
+#4 boolean
+is_boy= True
+
+#5 list 
+list1= [100,200,300]
+
+#6 dictinary 
+dict1= { 'name ':'souradeep', 'age': '25', 'city':'kolkata' }
+
+print(age)
+print(weight)
+print(name)
+print(is_boy)
+print(list1)
+print(dict1)
