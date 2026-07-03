e89307a31ab80abd05739bdb1079218dd080a54f
pradhan-not-found <souradeeppradhan7@gmail.com>
2026-07-03 12:43:04 +0530

    datatypes in python

--- datatypes.py
+++ datatypes.py
@@ -17,9 +17,37 @@ list1= [100,200,300]
 #6 dictinary 
 dict1= { 'name ':'souradeep', 'age': '25', 'city':'kolkata' }
 
+#7 tuple
+tuple1= (10,20,30)
+
+#8 set 
+set1= {1,2,3,4,5,6,7,8,9,10}
+
+#9 None 
+house_no= None
+
+#10 Complex
+com= 1+2j
+
+#11 range 
+num= range(1,5)
+
+#12 bytes & bytearray
+byte = bytes([65,66,97])
+byte_arra = bytearray([65,66,97])
+
+
+
 print(age)
 print(weight)
 print(name)
 print(is_boy)
 print(list1)
 print(dict1)
+print(tuple1)
+print(set1)
+print(house_no)
+print(com)
+print(num)
+print(byte)
+print(byte_arra)
